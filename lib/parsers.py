cb494772f1f44685b4f6b73bd7e976761cf066f9
Ivan Karl <ivan6258@gmail.com>
2014-07-21 21:49:10 -0300

    adds tests for ParsedString

--- lib/parsers.py
+++ lib/parsers.py
@@ -13,12 +13,19 @@ def parse_req(str_req):
 
 class ParsedString(object):
 
-    def __init__(self, **kwargs):
-        for key, value in kwargs.iteritems():
+    def __init__(self, attrs):
+        for key, value in attrs.iteritems():
             setattr(self, key, value)
 
+    def attribute_names(self):
+        return self.__dict__.keys()
+
+    def all_values(self):
+        return self.__dict__.values()
+
     def is_empty(self):
-        return not(any(self.__dict__.values()))
+        return not(any(self.all_values()))
+
 
 def parse_lot(str_lot):
     # TODO: implement parsed lot data object
--- test/parsers_test.py
+++ test/parsers_test.py
@@ -1,5 +1,6 @@
 import unittest
 from lib import parsers
+from lib.parsers import ParsedString
 
 
 class TestReqParser(unittest.TestCase):
@@ -18,21 +19,48 @@ class TestReqParser(unittest.TestCase):
 
 
 class TestLotParser(unittest.TestCase):
-    invalid_data = {'empty': '',
-                    'only_words': 'no numbers here',
-                    'symbols': '*yo #mamma so* fat# ** ## ***'}
-    valid_data = {'only_numbers': '5 90 34 88',
-                  'only_pfs': '*34 *76 *32 *342',
-                  'numbers_pfs': '34 *12 456 230 *90',
-                  'symbols': '#30'}
-    mixed_data = {'numbers_words': '45 09 tres 88 ilegal 456',
-                  'numbers_words_symbols': '*45  09 tres 88 ilegal 456 #45',
-                  'two_hashtags': '#35  09 tres 88 ilegal 456 #45'}
+
+    def setUp(self):
+        self.invalid_data = {'empty': '',
+                             'only_words': 'no numbers here',
+                             'symbols': '*yo #mamma so* fat# ** ## ***'}
+        self.valid_data = {'only_numbers': '5 90 34 88',
+                           'only_pfs': '*34 *76 *32 *342',
+                           'numbers_pfs': '34 *12 456 230 *90',
+                           'symbols': '#30'}
+        self.mixed_data = {'numbers_words': '45 09 tres 88 ilegal 456',
+                           'numbers_words_symbols': '*45  09 tres 88 ilegal 456 #45',
+                           'two_hashtags': '#35  09 tres 88 ilegal 456 #45'}
 
     def test_empty_data(self):
         # parsed = parsers.parse_req(invalid_data['empty'])
         # self.assert
         return self.skipTest('not implemented yet')
+
+
+class TestParsedString(unittest.TestCase):
+
+    def setUp(self):
+        self.values = {
+            'empty': {'a': '', 'b': ''}
+        }
+
+    def test_attribute_names(self):
+        parsed = ParsedString(self.values['empty'])
+        self.assertListEqual(
+            self.values['empty'].keys(),
+            parsed.attribute_names())
+
+    def test_all_values(self):
+        parsed = ParsedString(self.values['empty'])
+        self.assertListEqual(
+            self.values['empty'].values(),
+            parsed.all_values())
+
+    # def test_creates_object_with_attributes(self):
+    #     parsed = ParsedString(self.values['empty'])
+    #     self.assertTrue(parsed.is_empty())
+
 
 if __name__ == '__main__':
     unittest.main()
