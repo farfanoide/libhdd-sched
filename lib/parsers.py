2a0909f20c171361ca2e9c624c5f1b70c1fe4699
Ivan Karl <ivan6258@gmail.com>
2014-07-21 21:09:59 -0300

    adds ParsedString class

--- lib/parsers.py
+++ lib/parsers.py
@@ -1,13 +1,24 @@
 import re
 
+
 def parse_req(str_req):
     pf_str = re.compile('\*')
     if pf_str.match(str_req):
         req = (int(pf_str.sub('', str_req)), True)
     else:
         req = (int(str_req), False)
+
     return req
 
+
+class ParsedString(object):
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.iteritems():
+            setattr(self, key, value)
+
+    def is_empty(self):
+        return not(any(self.__dict__.values()))
 
 def parse_lot(str_lot):
     # TODO: implement parsed lot data object
--- test/parsers_test.py
+++ test/parsers_test.py
@@ -1,6 +1,7 @@
 import unittest
 from lib import parsers
 
+
 class TestReqParser(unittest.TestCase):
 
     def test_not_pf_and_value(self):
@@ -17,24 +18,20 @@ class TestReqParser(unittest.TestCase):
 
 
 class TestLotParser(unittest.TestCase):
-    invalid_data = {
-            'empty': '',
-            'only_words': 'no numbers here',
-            'symbols': '*yo #mamma so* fat# ** ## ***'
-            }
-    valid_data = {
-            'only_numbers': '5 90 34 88',
-            'only_pfs': '*34 *76 *32 *342',
-            'numbers_pfs': '34 *12 456 230 *90',
-            'symbols': '#30'
-            }
-    mixed_data = {
-            'numbers_words': '45 09 tres 88 ilegal 456',
-            'numbers_words_symbols': '*45  09 tres 88 ilegal 456 #45',
-            'two_hashtags': '#35  09 tres 88 ilegal 456 #45',
-            }
+    invalid_data = {'empty': '',
+                    'only_words': 'no numbers here',
+                    'symbols': '*yo #mamma so* fat# ** ## ***'}
+    valid_data = {'only_numbers': '5 90 34 88',
+                  'only_pfs': '*34 *76 *32 *342',
+                  'numbers_pfs': '34 *12 456 230 *90',
+                  'symbols': '#30'}
+    mixed_data = {'numbers_words': '45 09 tres 88 ilegal 456',
+                  'numbers_words_symbols': '*45  09 tres 88 ilegal 456 #45',
+                  'two_hashtags': '#35  09 tres 88 ilegal 456 #45'}
 
     def test_empty_data(self):
+        parsed = parsers.parse_req(invalid_data['empty'])
+        # self.assert
         return self.skipTest('not implemented yet')
 
 if __name__ == '__main__':
