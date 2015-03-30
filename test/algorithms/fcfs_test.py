51426a91465e9a2509f120d122afdbe8acb92381
Ivan Karl <ivan6258@gmail.com>
2015-03-30 00:28:18 -0300

    test cleanup

--- test/algorithms/fcfs_test.py
+++ test/algorithms/fcfs_test.py
@@ -2,16 +2,15 @@ import unittest
 import json
 from lib.algorithms import FCFS
 from lib.simulation import Simulation, SimulationResult
-from lib.parsers import parse_requirement, parse_lot
+from lib.parsers import parse_lot
+
+
 class TestFcfs(unittest.TestCase):
 
     simulation_dict = json.loads(file.read(open('./examples/protosimulation.json')))
     simulation = Simulation(simulation_dict)
-    # expected = ['*500', '*400', '*100', '53', '151', '33', '353', '100', '455', '15', '101', '126', '366', '415']
-    # expected_reqs = [parse_requirement(req) for req in expected]
     expected = '*500 *400 *100 53 151 33 353 100 455 15 101 126 366 415'
     expected_reqs = parse_lot(expected).requirements
-
 
     def setUp(self):
         self.results = FCFS.execute(self.simulation)
@@ -20,7 +19,9 @@ class TestFcfs(unittest.TestCase):
         self.assertIsInstance(self.results, SimulationResult)
 
     def test_fcfs_attended_requirements_order(self):
-        self.assertEquals(self.results.attended_requirements, self.expected_reqs)
+        self.assertEquals(
+            self.results.attended_requirements,
+            self.expected_reqs)
 
     def test_fcfs_default_behaviour(self):
         self.assertTrue(self.results.success)
--- test/lot_test.py
+++ test/lot_test.py
@@ -8,9 +8,8 @@ class TestLot(unittest.TestCase):
     data = { 'numbers_pfs': '34 *12 456 230 *90' }
 
     def test_lot_type(self):
-        # TODO: rethink basic tests for this class
         lot = parsers.parse_lot(self.data['numbers_pfs'])
-        self.assertIsInstance(lot, Lot)
+        self.assertIsInstance(lot.requirements, list)
 
 if __name__ == '__main__':
     unittest.main()
