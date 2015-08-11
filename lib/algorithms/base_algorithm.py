0be64ae859d102f006597d29e3e9744023d41743
Matias <chudix@localhost.localdomain>
2015-08-11 20:47:38 -0300

    WIP: adds base algorithm class

---
+++ lib/algorithms/base_algorithm.py
@@ -0,0 +1,12 @@
+class BaseAlgorithm(object):
+    """ An abstract class wich defines
+    functionality shared among algorithms
+    """
+
+    pfs = []
+    reqs = []
+
+    
+    @staticmethod
+    def get_pfs(a_lot):
+        pass
--- lib/algorithms/fcfs.py
+++ lib/algorithms/fcfs.py
@@ -1,12 +1,25 @@
-from lib.simulation import SimulationResult
-from lib.parsers import parse_requirement
+import json
+from lib.parsers import *
+
+from lib.simulation import Simulation, SimulationResult
+
+
+
+
+
 
 class FCFS():
+    
+    example = json.loads(file.read(open('./examples/protosimulation.json')))
+    simulation = Simulation(example)
 
-    @staticmethod
-    def execute(simulation):
-        expected = ['*500', '*400', '*100', '53', '151', '33', '353', '100', '455', '15', '101', '126', '366', '415']
-        expected_reqs = [parse_requirement(req) for req in expected]
-        return SimulationResult({'success': True,
-                                 'attended_requirements': expected_reqs
-                                 })
+
+    def execute(self, simulation):
+       expected = ['*500', '*400', '*100', '53', '151', '33', '353', '100', '455', '15', '101', '126', '366', '415']
+       expected_reqs = [parse_requirement(req) for req in expected]
+       result_dict = {'success': False,
+                       'attended_requirements': expected_reqs
+                   }
+        
+       result_dict['success'] = True
+       return SimulationResult(result_dict)
