3b8fd09339e07d214bae3b5561f2300926eb7e7d
Ivan Karl <ivan6258@gmail.com>
2015-08-19 20:21:50 -0300

    extracts last_attended to a method

--- lib/algorithms/base_algorithm.py
+++ lib/algorithms/base_algorithm.py
@@ -12,7 +12,6 @@ class BaseAlgorithm(object):
         self.simulation = simulation
         self.page_faults = []
         self.unattended = []
-        self.last_attended = simulation.position
         self.attended = []
         self.total_movs = 0
 
@@ -42,9 +41,14 @@ class BaseAlgorithm(object):
                 if self._exhausted_movements():
                     break
 
+    def _last_attended(self):
+        try:
+            return self.attended[-1]
+        except IndexError:
+            return self.simulation.position
+
     def _attend_req(self, req):
-        distance = self._distance(req, self.last_attended)
-        self.last_attended = req # TODO: get dinamically from self.attended[-1]
+        distance = self._distance(req, self._last_attended())
         self.total_movs += distance
         self.movements -= distance
         self.attended.append(req)
